64d29b7b42280297cbea0494486d7f575cce0926
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-11-14 15:51:07 +0530

    Snippet

--- js/script.js
+++ js/script.js
@@ -36,8 +36,8 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }, step);
     }
-    counter("count1", 0, 1287, 3000);
-    counter("count2", 100, 5786, 2500);
-    counter("count3", 0, 1440, 3000);
-    counter("count4", 0, 7110, 3000);
+    counter("count1", 2006, 6, 3000);
+    counter("count2", 2200, 200, 3000);
+    counter("count3", 0, 2000, 3000);
+    counter("count4", 2500, 500, 3000);
  });
