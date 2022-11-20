61d6cd7c0a3ff27bc348c0d20f76cd5e8730e5e9
Ankur Gupta <82110808+ankurgupta032@users.noreply.github.com>
2022-11-20 17:03:18 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let nav = document.querySelector(".navbar");
 window.onscroll = function() {
     if(document.documentElement.scrollTop > 50){
         nav.classList.add("header-scrolled"); 
-        document.getElementById("navvvv").style.backgroundColor = "#DDFFF7";
+        document.getElementById("navvvv").style.backgroundColor = "white";
     }else{
         nav.classList.remove("header-scrolled");
         document.getElementById("navvvv").style.backgroundColor = "transparent";
