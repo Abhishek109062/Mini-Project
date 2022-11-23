f8c69b8151084cea07b021d1c5e7d5e40d5d7a56
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-11-24 00:55:01 +0530

    Complete

--- js/script.js
+++ js/script.js
@@ -22,24 +22,3 @@ navBar.forEach(function(a){
     })
 })
 
-// counter design
-document.addEventListener("DOMContentLoaded", () => {
-    function counter(id, start, end, duration){
-        let obj = document.getElementById(id),
-        current = start,
-        range = end - start,
-        increment = end > start ? 1 : -1,
-        step = Math.abs(Math.floor(duration /  range)),
-        timer = setInterval(() => {
-            current += increment;
-            obj.textContent = current;
-            if(current == end){
-                clearInterval(timer);
-            }
-        }, step);
-    }
-    counter("count1", 0, 6, 3000);
-    counter("count2", 0, 20, 3000);
-    counter("count3", 0, 2000, 3000)
-    counter("count4", 0, 500, 3000);
- });
